0333339d733a92eba3d2480994999fbe67e4f0bd
Igor Leandro Dantas Medeiros <igtag.code@gmail.com>
2025-06-05 09:08:50 -0300

    feat(js): Add new `js` files

---
+++ javascript/app.js
@@ -0,0 +1,26 @@
+console.log("Hello World!"); //Aula1
+/* ======================
+          JS Basics
+==========================*/
+
+// const HeadingOne = document.querySelector(".heading-primary");
+// const myName = "Igor Dantas";
+// console.log(myName);
+
+// HeadingOne.addEventListener("click", () => {
+//   HeadingOne.style.color = "purple";
+//   HeadingOne.style.backgroundColor = "red";
+//   HeadingOne.style.padding = "1.6rem";
+//   HeadingOne.style.borderRadius = "2rem";
+//   HeadingOne.innerText = myName;
+// });
+
+/*======================= 
+  Footer DateYear Updated
+  ======================= 
+*/
+
+const YearNowEl = document.querySelector("#YearNowDate");
+const YearUpdated = new Date().getFullYear();
+console.log(YearUpdated);
+YearNowEl.textContent = YearUpdated;
